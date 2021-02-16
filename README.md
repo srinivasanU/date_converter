73706d226d05eac61c61d8045594063b6534b59c
srinivasan <1694srinivasan@gmail.com>
2021-02-16 16:48:36 +0530

    additional date conversion logics

--- README.md
+++ README.md
@@ -9,15 +9,35 @@ Get Date conversion for your app
 Then...
 
 ```
-import { minusindiandate,slashindiandate } from 'dateconvertermethods';
+import { minusindiandate,slashindiandate,minusshortdateindian,slashshortdateindian,minusfulldateglobal,minusshortdateglobal,slashfulldateglobal,slashshortdateglobal,mediumdate,longdate,shortfulldate,longfulldate } from 'dateconvertermethods';
 
 minusindiandate(new Date())
 slashindiandate(new Date())
+minusshortdateindian(new Date())
+slashshortdateindian(new Date())
+minusfulldateglobal(new Date())
+minusshortdateglobal(new Date())
+slashfulldateglobal(new Date())
+slashshortdateglobal(new Date())
+mediumdate(new Date())
+longdate(new Date())
+shortfulldate(new Date())
+longfulldate(new Date())
 ```
 
 # Options
 
-Date Convertor supports 2 options:
+Date Convertor supports these options:
 
 * *minusindiandate* - _dd-mm-yyyy_ 
 * *slashindiandate* - _dd/mm/yyyy_ 
+* *minusshortdateindian* - _dd-mm-yy_ 
+* *slashshortdateindian* - _dd/mm/yy_ 
+* *minusfulldateglobal* - _mm-dd-yyyy_ 
+* *minusshortdateglobal* - _mm-dd-yy_ 
+* *slashfulldateglobal* - _mm/dd/yyyy_
+* *slashshortdateglobal* - _mm/dd/yy_ 
+* *mediumdate* - _ShortMonthName dd , yyyy_ 
+* *longdate* - _LongMonthName dd , yyyy_ 
+* *shortfulldate* - _ShortDayName , ShortMonthName dd , yyyy_
+* *longfulldate* - _LonggayName , _LongMonthName dd , yyyy_
--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-module.exports.minusindiandate = date => {
+module.exports.minusfulldateindian = date => {
       var userdate = date.toString()
       var d = new Date(userdate);
       var ye = d.getFullYear();
@@ -8,12 +8,102 @@ module.exports.minusindiandate = date => {
       return finaldate
 }
 
-module.exports.slashindiandate = date => {
+module.exports.slashfulldateindian = date => {
       var userdate = date.toString()
       var d = new Date(userdate);
       var ye = d.getFullYear();
       var da = d.getDate();
       var mo = d.getMonth()+1;
       var finaldate = ('0' + da.toString()).slice(-2)+"/"+('0' + mo.toString()).slice(-2)+"/"+ye.toString()
+      return finaldate
+}
+
+module.exports.minusshortdateindian = date => {
+      var userdate = date.toString()
+      var d = new Date(userdate);
+      var ye = d.getFullYear();
+      var da = d.getDate();
+      var mo = d.getMonth()+1;
+      var finaldate = ('0' + da.toString()).slice(-2)+"-"+('0' + mo.toString()).slice(-2)+"-"+ye.toString().slice(-2)
+      return finaldate
+}
+      
+module.exports.slashshortdateindian = date => {
+      var userdate = date.toString()
+      var d = new Date(userdate);
+      var ye = d.getFullYear();
+      var da = d.getDate();
+      var mo = d.getMonth()+1;
+      var finaldate = ('0' + da.toString()).slice(-2)+"/"+('0' + mo.toString()).slice(-2)+"/"+ye.toString().slice(-2)
+      return finaldate
+}
+
+
+module.exports.minusfulldateglobal = date => {
+      var userdate = date.toString()
+      var d = new Date(userdate);
+      var ye = d.getFullYear();
+      var da = d.getDate();
+      var mo = d.getMonth()+1;
+      var finaldate = ('0' + mo.toString()).slice(-2)+"-"+('0' + da.toString()).slice(-2)+"-"+ye.toString()
+      return finaldate
+}
+
+module.exports.minusshortdateglobal = date => {
+      var userdate = date.toString()
+      var d = new Date(userdate);
+      var ye = d.getFullYear();
+      var da = d.getDate();
+      var mo = d.getMonth()+1;
+      var finaldate = ('0' + mo.toString()).slice(-2)+"-"+('0' + da.toString()).slice(-2)+"-"+ye.toString().slice(-2)
+      return finaldate
+}
+
+module.exports.slashfulldateglobal = date => {
+      var userdate = date.toString()
+      var d = new Date(userdate);
+      var ye = d.getFullYear();
+      var da = d.getDate();
+      var mo = d.getMonth()+1;
+      var finaldate = ('0' + mo.toString()).slice(-2)+"/"+('0' + da.toString()).slice(-2)+"/"+ye.toString()
+      return finaldate
+}
+
+module.exports.slashshortdateglobal = date => {
+      var userdate = date.toString()
+      var d = new Date(userdate);
+      var ye = d.getFullYear();
+      var da = d.getDate();
+      var mo = d.getMonth()+1;
+      var finaldate = ('0' + mo.toString()).slice(-2)+"/"+('0' + da.toString()).slice(-2)+"/"+ye.toString().slice(-2)
+      return finaldate
+}
+
+
+module.exports.mediumdate = date => {
+      var userdate = date.toString()
+      var d = new Date(userdate);
+      var finaldate = d.toLocaleString('default', { month: 'short' }) + ' ' + ('0' + d.getDate().toString()).slice(-2) + ', ' + d.getFullYear().toString() 
+      return finaldate
+}
+
+module.exports.longdate = date => {
+      var userdate = date.toString()
+      var d = new Date(userdate);
+      var finaldate = d.toLocaleString('default', { month: 'long' }) + ' ' + ('0' + d.getDate().toString()).slice(-2) + ', ' + d.getFullYear().toString() 
+      return finaldate
+}
+
+module.exports.shortfulldate = date => {
+      var userdate = date.toString()
+      var d = new Date(userdate);
+      var finaldate = d.toLocaleString('en-us', {weekday:'short'}).toString() + ', ' + d.toLocaleString('default', { month: 'short' }) + ' ' + ('0' + d.getDate().toString()).slice(-2) + ', ' + d.getFullYear().toString() 
+      return finaldate
+}
+
+module.exports.longfulldate = date => {
+      var userdate = date.toString()
+      var d = new Date(userdate);
+      var finaldate = d.toLocaleString('en-us', {weekday:'long'}).toString() + ', ' + d.toLocaleString('default', { month: 'long' }) + ' ' + ('0' + d.getDate().toString()).slice(-2) + ', ' + d.getFullYear().toString() 
       return finaldate
 }
